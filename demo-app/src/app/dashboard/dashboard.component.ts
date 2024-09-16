4a05dd15d3ffbd7889c1d08ff279df4a1ae2acb4
pmattas <pmattas@extremenetworks.com>
2024-09-16 15:13:24 +0530

    Changes for navigation

--- demo-app/src/app/dashboard/dashboard.component.ts
+++ demo-app/src/app/dashboard/dashboard.component.ts
@@ -2,11 +2,12 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsComponent } from '../forms/forms.component';
 import { ChartsComponent } from '../charts/charts.component';
+import { NavigationComponent } from "../navigation/navigation.component";
 
 @Component({
   selector: 'demo-app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavigationComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
---
+++ demo-app/src/app/navigation/navigation.component.ts
@@ -0,0 +1,12 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'demo-app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss'],
+  standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class NavigationComponent {
+
+}
