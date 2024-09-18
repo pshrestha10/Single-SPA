0d33c42c24defbc706edf5ede6e4256f426b7951
pshrestha10 <pshrestha@extremenetworks.com>
2024-09-18 10:12:41 +0530

    sorting functionality

--- demo-app/src/app/table/table.component.ts
+++ demo-app/src/app/table/table.component.ts
@@ -16,7 +16,7 @@ export class TableComponent implements OnInit {
     { headerName: 'S N', valueGetter: 'node.rowIndex + 1', flex: 1 },
     { field: 'id', headerName: 'ID', flex: 1 },
     { field: 'name', headerName: 'Name', flex: 2 },
-    { field: 'gpa', headerName: 'GPA', flex: 1 },
+    { field: 'gpa', headerName: 'GPA', flex: 1, sortable: true,},
     { field: 'email', headerName: 'Email', flex: 2 },
     { field: 'phone', headerName: 'Phone', flex: 1 },
     { field: 'actions', headerName: 'Actions', flex: 1 }
@@ -45,6 +45,15 @@ export class TableComponent implements OnInit {
       }
     );
   }
+
+  // applyFilter(event: Event): void {
+  //   const input = event.target as HTMLInputElement;
+  //   const filterValue = input.value.trim().toLowerCase();
+  //   this.storedData.filterPredicate = (data: any, filter: string) => {
+  //     return data.name.toLowerCase().includes(filter);
+  //   };
+  //   this.storedData.filter = filterValue;
+  // }
   // addStudent(){
   //   const dialogRef = this.dialog.open(DialogComponent, {
   //     data: { showChart: 0 },
