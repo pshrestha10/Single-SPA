e9213021aa0161bf7edc37e882818f7b695c0bf7
pmattas <pmattas@extremenetworks.com>
2024-09-18 21:34:09 +0530

    Changes for pagination

--- demo-app/src/app/table/table.component.ts
+++ demo-app/src/app/table/table.component.ts
@@ -2,17 +2,26 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { AgGridModule } from 'ag-grid-angular';
 import { Students } from '../students.services';
 import { DialogComponent } from '../dialog/dialog.component';
+import { PaginationNumberFormatterParams } from 'ag-grid-community';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'demo-app-table',
   standalone: true,
-  imports: [AgGridModule, DialogComponent],
+  imports: [AgGridModule, DialogComponent,CommonModule],
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class TableComponent implements OnInit {
   private gridApi: any;
+  public paginationPageSize = 10;
+  public paginationPageSizeSelector:  number[] = [5, 10, 20, 40];;
+  public paginationNumberFormatter: (
+    params: PaginationNumberFormatterParams,
+  ) => string = (params: PaginationNumberFormatterParams) => {
+    return params.value.toLocaleString();
+  };
   columnDefs = [
     { headerName: 'S N', valueGetter: 'node.rowIndex + 1', flex: 1 },
     { field: 'id', headerName: 'ID', flex: 1 },
@@ -25,8 +34,8 @@ export class TableComponent implements OnInit {
       flex: 1,
       cellRenderer: (params: any) => {
         return `
-          <en-button class="edit-btn" onclick="editStudent(${params.data.id})"><en-icon-edit></en-button>
-          <en-button class="delete-btn" onclick="deleteStudent(${params.data.id})"><en-icon-delete></en-button>
+            <en-button class="edit-btn" onclick="editStudent(${params.data.id})"><en-icon-edit></en-button>
+            <en-button class="delete-btn" onclick="deleteStudent(${params.data.id})"><en-icon-delete></en-button>
         `;
       },
     }    
@@ -79,6 +88,13 @@ export class TableComponent implements OnInit {
     localStorage.setItem('studentsData', JSON.stringify(this.rowData));
   }
 
+  // onPageSizeChange(event: Event): void {
+  //   const selectElement = event.target as HTMLSelectElement;
+  //   this.paginationPageSize = Number(selectElement.value);
+  //   if (this.gridApi) {
+  //     this.gridApi.paginationSetPageSize(this.paginationPageSize);
+  //   }
+  // }
   // addStudent(){
   //   const dialogRef = this.dialog.open(DialogComponent, {
   //     data: { showChart: 0 },
