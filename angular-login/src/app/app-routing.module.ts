70788686859266ec54f986a1730da219c1e850c2
pmattas <pmattas@extremenetworks.com>
2024-09-26 00:03:08 +0530

    Changes for improvements

--- angular-login/src/app/app-routing.module.ts
+++ angular-login/src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { EmptyRouteComponent } from './empty-route/empty-route.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ForgotpwdComponent } from './forgotpwd/forgotpwd.component';
+import { SecurityComponent } from './security/security.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent},
+  { path: 'security', component: SecurityComponent},
   { path: 'forgotpwd', component: ForgotpwdComponent}
 ];
 
